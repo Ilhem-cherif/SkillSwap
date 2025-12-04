064f6e844ec2b2b5115288cbbd9ef70daa105b18
Ilhem-cherif <cherifilhem17@gmail.com>
2025-12-04 14:18:50 +0100

    add AccountRepository class

---
+++ credits-service/credits-service/src/main/java/com/skillswap/credits_service/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.skillswap.credits_service.repository;
+
+import com.skillswap.credits_service.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
+}
